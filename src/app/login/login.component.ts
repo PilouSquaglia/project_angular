0f09096e8b2819b5d0201dc4fbfed0611d36860f
Pierre-Louis Squaglia <pl.squaglia@gmail.com>
2022-12-01 15:53:58 +0100

    login feature

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
@@ -13,6 +14,7 @@ export class LoginComponent {
   email = new FormControl('');
   password = new FormControl('');
   loginForm = new FormGroup('');
+  login$: Observable<any> | undefined;
 
   checkoutForm = this.formBuilder.group({
     email:this.email,
@@ -20,7 +22,8 @@ export class LoginComponent {
   })
 
   constructor(private loginService: LoginService,
-              private formBuilder: FormBuilder){
+              private formBuilder: FormBuilder,
+              private httpClient : HttpClient){
   }
 
   ngOnInit(): void {
@@ -31,7 +34,9 @@ export class LoginComponent {
     // console.warn('Your order has been submitted', this.checkoutForm.value);
     console.log("#1");
     console.log(this.checkoutForm.value);
-    this.loginService.postLogin(this.checkoutForm.value);
+    // this.httpClient.postLogin(this.checkoutForm.value);
+    // this.httpClient.post('http://localhost:3000/login/test', this.checkoutForm.value)
+    this.httpClient.post<any>('http://localhost:3000/login', this.checkoutForm.value).subscribe();
     this.checkoutForm.reset();
   }
 }
--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -6,16 +6,17 @@ import { Injectable } from '@angular/core';
 })
 export class LoginService {
 
-  constructor(private httpClient : HttpClient) { }
+  constructor(private httpClient : HttpClient) {}
 
   loginUsers(){
     return this.httpClient.get('http://localhost:3000/login');
   }
 
   postLogin(values : {}){
-    console.log("#2")
-    let values_post = JSON.stringify(values)
-    console.log(values_post);
-    return this.httpClient.post('http://localhost:3000/login', values_post)
+    console.log("#2");
+    console.log(values);
+    // let values_post = JSON.stringify(values);
+    // console.log(values_post);
+    return this.httpClient.post('http://localhost:3000/login/test', values);
   }
 }
